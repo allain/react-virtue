cfafb328aadf0438e60848c71ddad8fe21da75a9
allain <allain.lalonde@favequest.com>
2017-09-12 15:54:00 -0400

    not transpiling for older browsers

--- examples/list-random.js
+++ examples/list-random.js
@@ -1,6 +1,6 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-const Virtue = require('..')
+const Virtue = require('../src/Virtue')
 
 module.exports = function run() {
   const randomWords = {}
